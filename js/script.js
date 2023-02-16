219a8a9c4be9ca500c9010b968c29e20b8ae8908
Dustin Yochim <yochim.dustin@gmail.com>
2023-02-16 15:20:14 -0600

    Add functionality for adding/removing books

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-// Get the modal
-const bookshelf = [];
+const library = document.querySelector(".library");
+let books = document.querySelectorAll(".book");
+const modal = document.querySelector(".modal");
+const form = document.querySelector(".book-form");
 
+// Simple array to store book objects
+const myLibrary = [];
+
+// Write a constructor for making "book" objects
 class Book {
   constructor(title, author, pages, status) {
     this.title = title;
@@ -9,42 +15,122 @@ class Book {
     this.status = status;
   }
 }
-function addToBookshelf() {
-  const title = prompt("Title");
-  const author = prompt("Author");
-  const pages = prompt("Pages");
-  const status = prompt("Status");
+
+// Add a function that can take user's input and store the new book objects into myLibrary
+function addToLibrary(title, author, pages, status) {
   const newBook = new Book(title, author, pages, status);
-  bookshelf.push(newBook);
+  myLibrary.push(newBook);
 }
 
-function showBookshelf() {
-  bookshelf.forEach((book) => {
-    console.log(book);
+// Shows the form to add a book
+function showForm() {
+  modal.setAttribute(
+    "style",
+    "display: flex; justify-content: center; align-items: center;"
+  );
+}
+
+// Hides the form to add a book
+function hideForm() {
+  form.reset();
+  modal.style.display = "none";
+}
+
+// Shows books from myLibrary on page
+function displayBooks() {
+  // Remove Books from Page
+  books = document.querySelectorAll(".book");
+  books.forEach((item) => {
+    item.remove();
+  });
+  // Add books back to page
+  myLibrary.forEach((book) => {
+    const newBook = document.createElement("div");
+    newBook.classList.add("book");
+
+    const newBookTitle = document.createElement("div");
+    newBookTitle.classList.add("book-title");
+    newBookTitle.textContent = `Title: ${book.title}`;
+    newBook.appendChild(newBookTitle);
+
+    const newBookAuthor = document.createElement("div");
+    newBookAuthor.classList.add("book-author");
+    newBookAuthor.textContent = `Author: ${book.author}`;
+    newBook.appendChild(newBookAuthor);
+
+    const newBookPages = document.createElement("div");
+    newBookPages.classList.add("book-pages");
+    newBookPages.textContent = `Pages: ${book.pages}`;
+    newBook.appendChild(newBookPages);
+
+    const newBookStatus = document.createElement("div");
+    newBookStatus.classList.add("book-status");
+    newBookStatus.textContent = `Status: ${book.status}`;
+    newBook.appendChild(newBookStatus);
+
+    const removeBookButton = document.createElement("button");
+    removeBookButton.classList.add("remove-book-button");
+    removeBookButton.textContent = "Remove";
+    removeBookButton.dataset.index = myLibrary.indexOf(book);
+    newBook.appendChild(removeBookButton);
+
+    library.appendChild(newBook);
+  });
+}
+
+// Sets event listeners
+function addEventListeners() {
+  const btn = document.querySelector(".add-book");
+  btn.addEventListener("click", showForm);
+
+  const span = document.querySelector(".close");
+  span.addEventListener("click", hideForm);
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      hideForm();
+    }
+  });
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const title = form.elements.title.value;
+    const author = form.elements.author.value;
+    const pages = form.elements.pages.value;
+    const status = form.elements.status.value;
+    if (title !== "" && author !== "" && pages !== "" && status !== "") {
+      addToLibrary(title, author, pages, status);
+      hideForm();
+      displayBooks();
+      addEventListeners();
+    }
+  });
+
+  const removeButtons = document.querySelectorAll(".remove-book-button");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      myLibrary.splice(button.dataset.index, 1);
+      displayBooks();
+      addEventListeners();
+    });
   });
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-bookshelf.push(theHobbit);
+const theGiver = new Book("The Giver", "J.R.R. Tolkien", 295, "not read yet");
+const otherBook = new Book(
+  "The Hitchhiker's Guide to the Galaxy",
+  "J.R.R. Tolkien",
+  295,
+  "not read yet"
+);
 
-// addToBookshelf();
-showBookshelf();
+myLibrary.push(theHobbit);
+myLibrary.push(theGiver);
+myLibrary.push(otherBook);
+function main() {
+  displayBooks();
+  addEventListeners();
+}
 
-// MODAL
-const modal = document.querySelector(".modal");
-const btn = document.querySelector(".add-book");
-const span = document.querySelector(".close");
-console.log(span);
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+main();
